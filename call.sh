e6d5b916e41f853d0f43caa35391176d1c80db53
CR4CK3D <50992202+CR4CK3D@users.noreply.github.com>
2019-05-30 16:24:54 +0700

    Add files via upload

---
+++ call.sh
@@ -0,0 +1,22 @@
+#!/system/xbin/bash
+clear
+echo "\033[36;1m"
+figlet "Fake Call"
+sleep 1
+echo "\033[31;1m Fake Call Tokoped"
+sleep 1
+echo "\033[33;1m Author: MR.CRACKED"
+sleep 1
+echo "\033[32;1m contact: +37121599164"
+sleep 1
+echo "\033[34;1m ただ楽しみのために！"
+sleep 1
+echo "Ketik 1 Untuk Lanjut"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "Fake Call"
+php 1.php
+fi
