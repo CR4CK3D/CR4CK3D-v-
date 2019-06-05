2a0248ee80b995e5e6148809c1495d9a3f425f1b
CR4CK3D <50992202+CR4CK3D@users.noreply.github.com>
2019-06-05 15:20:47 +0700

    Add files via upload

---
+++ …….sh
@@ -0,0 +1,15 @@
+toilet -f mono12 -F metal "Login"
+read -p "Masukan password : " pass
+if [ $pass = "CRACKED" ]
+then
+clear
+toilet -f mono12 -F metal "Open"
+sleep 1
+clear
+else
+  echo "\033[31;1mKalau nggak tahu tanya Cracked"
+  echo "\033[31;1mIni No Wa. +37121599164"
+  echo "\033[37;1m"
+  sleep 3
+  sh …….sh
+fi
